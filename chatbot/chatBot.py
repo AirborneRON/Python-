332b19ffa9d07f732771004c6c7a1b904a25cacf
AirborneRON <azzzz72@googlemail.com>
2016-11-13 23:50:24 +0000

    done

--- chatbot/chatBot.py
+++ chatbot/chatBot.py
@@ -5,7 +5,7 @@ file.close()
 #seperating each element of the list by the return key 
 #then close
 
-
+# all responses should start with a space before typing
 
 print(" Hello ")    
 response = raw_input(" what is your name ?")
@@ -19,16 +19,15 @@ for nextWord in words:
 #with the String  
         
 #print ("line 21" + nextWord)        
-if (response == "aaron"):
-    print("how is that pronounced if you dont mind me asking")
-    #if ( response ==)
-    #elif(response == "")
+response = raw_input ("how lovely to meet you")
+
+if (response == "my names aaron"):
+    print("how is that pronounced if you dont mind me asking ? ")
+    
 
 
-response = raw_input( "not how it sounds then, my names Mac btw")
-
-#print ("line 30" + response)
-if (response == "nice to meet you"):
+response = raw_input( " Interesting name btw, my names Mac")
+if (response == " nice to meet you"):
     print("likewise")
 
 
@@ -38,20 +37,27 @@ if (response == "im from cornwall originally"):
 
 #if (response == "")
 
-response = raw_input("is there anywhere you'd want to go for a coffee down there ?")
-print(response)
+response = raw_input("is there anywhere you'd want to go for a coffee there ?")
+if (response == " yes"):
+    print("Great I look forward to it")
+elif(response == " no"):
+    print("sod you then" + " i'll go by myself")
 
-response = raw_input("so how old are you ?")
-print("response")
+response = raw_input("anyways, so how old are you ?")
+if (response == " 18"):
+    print(" not as old as me then ")
+elif (response == " 23"):
+    print("same age as me then")
+
 
 
 response = raw_input(" whats your favourite colour ?")
 if (response == "blue"):
     print("thats mine too")
 elif(response == "red"):
-    print("red is sick" )
+    print("red is sick" + " but unfortunetly we must end this conversation" )
     
 elif(response == "yellow"):
-    print ("yellows pretty cool too ")
+    print ("yellows pretty cool too " + " anyways i really must be off TTFN")
     
-else: print("all other colours suck")    
+else: print("im not a fan of that colour" + "and on that note good day to you sir")    
